b9868067b0aec8a26e07d5dd88c58dca22ded059
Jenny Larruy <jenny.larruy@gmail.com>
2021-06-22 16:24:10 +0200

    mine and minePosition

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,49 @@ import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
-        terminalFactory.setInitialTerminalSize(new TerminalSize(80,40));
+        terminalFactory.setInitialTerminalSize(new TerminalSize(80, 40));
         Terminal terminal = terminalFactory.createTerminal();
         terminal.setCursorVisible(false);
+
+        List<Mine> mineList = new ArrayList<>();
+
+        //gör nya mines... när?
+        //ge minePosition random coordinates beroende på väggarnas coordinates och byte ut?
+        if (true/*???*/){
+            mineList.add(new Mine(new MinePosition(ThreadLocalRandom.current().nextInt(60, 80), ThreadLocalRandom.current().nextInt(1, 40))))
+            terminal.setCursorPosition(mineList.get(0).getMinePosition().getX(), mineList.get(0).getMinePosition().getY());
+            terminal.putCharacter(Mine.MINE_FIG);
+            terminal.flush();
+        }
+
+        //move all mines in mineList
+        for (Mine m : mineList) {
+            m.move();
+            terminal.setCursorPosition(m.getMinePosition().getX(), m.getMinePosition().getY());
+            terminal.putCharacter(Mine.MINE_FIG);
+            terminal.setCursorPosition(m.getPrevMinePosition().getX(), m.getPrevMinePosition().getY());
+            terminal.putCharacter(' ');
+        }
+
+        //om minePosition x=1 -> försvinner ut till vänster
+        if (mineList.get(0).getMinePosition().getX() == 1) {
+            terminal.setCursorPosition(mineList.get(0).getMinePosition().getX(), mineList.get(0).getMinePosition().getY());
+            terminal.putCharacter(' ');
+            mineList.remove(0);
+        }
+
+
+
 /*
         player= new player
         wall = new wall
@@ -19,7 +54,7 @@ public class Main {
             walls.move
             walls.create
 
-            mine.move
+            mine.get(i).move();
             mine.create
 
             check if key pressed => move player
--- src/main/java/Mine.java
+++ src/main/java/Mine.java
@@ -1,2 +1,32 @@
 public class Mine {
+
+    final static char MINE_FIG = 'o';
+    MinePosition minePosition;
+    MinePosition prevMinePosition;
+
+    public Mine(MinePosition minePosition) {
+        this.minePosition=minePosition;
+        MinePosition prevMinePosition=new MinePosition(1, 1); //borde kunna vara samma från början??
+    }
+
+    public void move (){
+        prevMinePosition=minePosition;
+        minePosition.setX(minePosition.getX()-1);
+    }
+
+    public MinePosition getMinePosition() {
+        return minePosition;
+    }
+
+    public void setMinePosition(MinePosition minePosition) {
+        this.minePosition = minePosition;
+    }
+
+    public MinePosition getPrevMinePosition() {
+        return prevMinePosition;
+    }
+
+    public void setPrevMinePosition(MinePosition prevMinePosition) {
+        this.prevMinePosition = prevMinePosition;
+    }
 }
